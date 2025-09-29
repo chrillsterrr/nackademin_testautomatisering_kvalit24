214c5e863ab6fb545dc2fb6179a8a7bb5529a934
Christian Hasselgren <christianhfit@hotmail.com>
2025-09-29 11:02:37 +0200

    changes

--- labs/06_07_08_test_framework/models/ui/signup.py
+++ labs/06_07_08_test_framework/models/ui/signup.py
@@ -1,7 +1,9 @@
-class SignupPage:
-    def __init__(self, page):
-        self.page = page
+import os
 
+class SignupPage:
+    def __init__(self, page, base_url=None):
+        self.page = page
+        self.base_url = base_url or os.getenv("APP_URL", "http://localhost:5173")
         self.signup_input_username = page.get_by_placeholder('Username')
         self.signup_input_password = page.get_by_placeholder('Password')
         self.signup_btn_signup = page.locator('button.button-primary')
@@ -14,4 +16,4 @@ class SignupPage:
         self.signup_btn_signup.click()
 
     def go_to_home(self):
-        self.page.goto("http://localhost:5173")
+        self.page.goto(self.base_url)
